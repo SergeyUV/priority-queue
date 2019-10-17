36155d755d10ed5ddaef6945ba8c8c09abb0e02b
Sergey Vnukov <skivun@hotmail.com>
2019-10-17 15:21:49 +0300

    feat (class Node): add constructor and appendChild

--- src/node.js
+++ src/node.js
@@ -1,10 +1,22 @@
 class Node {
+	
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
+		if(this.left == null){
+			this.left = node;
+			return;	
+		}
 
+		if(this.right == null){
+			this.right = node;
+		}
 	}
 
 	removeChild(node) {
