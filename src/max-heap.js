c4187e4df52efa4d72a082b85d1b9684400e49cd
Sergey Vnukov <skivun@hotmail.com>
2019-10-18 16:28:56 +0300

    fix: maxHeap insertNode inserts nodes to correct places

--- src/max-heap.js
+++ src/max-heap.js
@@ -39,8 +39,28 @@ class MaxHeap {
 		
 		if(!this.root){
 			this.root = node;
+			return;
 		}
 
+		let queue = [this.root];
+		
+		while (true){
+			
+			if(queue[0].left == null){
+				queue[0].left = node;
+				node.parent = queue[0];
+				return;
+			}
+
+			if(queue[0].right == null){
+				queue[0].right = node;
+				node.parent = queue[0];
+				return;
+			}
+
+			queue.push(queue[0].left, queue[0].right);
+			queue.shift();
+		}
 	}
 
 	shiftNodeUp(node) {
