54711a2b35cc368fbedeb76aa411d3b0ea9d18c2
Sergey Vnukov <skivun@hotmail.com>
2019-10-20 19:18:37 +0300

    feat: add detachRoot()

--- src/max-heap.js
+++ src/max-heap.js
@@ -5,41 +5,87 @@ class MaxHeap {
 
 		this.root = null;
 		this.parentNodes = [];
+		this.size = 0;
+		this.lastInserted = null;
 	}
 
 	push(data, priority) {
 		let node = new Node(data, priority);
 		this.insertNode(node);
+		this.lastInserted = node.parent;
 		this.shiftNodeUp(node);
 	}
 
 	pop() {
+		this.size--;
+		if (!this.root)	{
+			return;
+		}
 		
+		let ret_val = this.root.data;
+
+		return ret_val;
 	}
 
 	detachRoot() {
+		console.log('parentnodes');
+		console.log(this.parentNodes);
+		if(!this.root){
+			return;
+		}
 		
+		if(this.parentNodes[0] === this.root){
+			this.parentNodes.shift();
+			this.detachedWasParent = true;
+		}else{
+			this.detachedWasParent = false;
+		}
+
+		let tmp = this.root;
+		this.root = null;
+		return tmp;
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
 		
+		this.root = this.lastInserted;
+		if(this.detachedWasParent){
+			this.parentNodes.unshift(this.root);
+		}
+		//console.log('last: ');
+		///console.log(Object.entries(this.lastInserted));
+		//console.log('det: ');
+		//console.log(Object.entries(detached));
+
+		this.root.left = detached.left;
+		this.root.left.parent = this.root;
+		this.root.right = detached.right;
+		this.root.right.parent = this.root;
+		
+
+		console.log('parentnodes');
+		console.log(this.parentNodes);
+
 	}
 
 	size() {
-		
+	
+		return this.size;
 	}
 
 	isEmpty() {
-		
+		return (this.size == 0)? true : false;
 	}
 
 	clear() {
 		this.root = null;
 		this.parentNodes = [];
+		this.size = 0;
 	}
 
 	insertNode(node) {
-		
+		this.size++;
+	
 		if(!this.root){
 			this.root = node;
 			this.parentNodes.push(this.root);
